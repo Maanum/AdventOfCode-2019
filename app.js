0cf48350264155c11cf4abe138a4257caa438365
Maanum <kristofer.maanum@gmail.com>
2019-12-12 19:26:26 +0530

    Convert tabs to spaces

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const fs = require('fs');
 const filePath = './input.txt';
 
 const getData = filePath => {
-	return new Promise((resolve, reject) => {
-		fs.readFile(filePath, 'utf-8', (err, data) => {
-		  if (err) throw err;
-		 	inputMass = data.split('\n');
-		 	resolve(inputMass);
-		});
-	})
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) throw err;
+       inputMass = data.split('\n');
+       resolve(inputMass);
+    });
+  })
 }
 
 getData(filePath)
-	.then(data => {
-		let fuel = data.map(mass => {
-			return Math.floor(mass / 3) - 2;
-		})
+  .then(data => {
+    let fuel = data.map(mass => {
+      return Math.floor(mass / 3) - 2;
+    })
 
-		let result = fuel.reduce((a, b) => a + b, 0);
-		console.log(result);
-	})
+    let result = fuel.reduce((a, b) => a + b, 0);
+    console.log(result);
+  })
 
 
 
